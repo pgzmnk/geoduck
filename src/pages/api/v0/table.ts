5bcfaf33ab18c7ed5bb4b22c6b0792485cdd3d13
pg <pg@pgs-MacBook-Pro.local>
2023-04-21 12:17:28 -0700

    Update table endpint

--- src/pages/api/v0/table.ts
+++ src/pages/api/v0/table.ts
@@ -1,26 +1,38 @@
+"use client";
+
 import type { NextApiRequest, NextApiResponse } from "next";
 import * as duckdb from "@duckdb/duckdb-wasm";
+import * as rd from "@duckdb/react-duckdb";
 
 type ResponseData = {
   message: string;
+  data?: Dataset[];
 };
 
-type PostRequestData = {
+type Dataset = {
   name: string;
   query: string;
 };
 
-export default function handler(
+const data: Dataset[] = [
+  {
+    name: "cpv_geo_ageb",
+    query:
+      "https://open-demo-datasets.s3.us-west-2.amazonaws.com/inegi/cpv_2020/geo_cpv_00_routerjoin.parquet",
+  },
+];
+
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseData>
 ) {
   try {
     switch (req.method) {
       case "GET":
-        res.status(200).json({ message: "GET method" });
+        res.status(200).json({ message: "GET method", data: data });
         break;
       case "POST":
-        res.status(200).json({ message: "POST method" });
+        res.status(200).json({ message: "POST method", data: data });
         break;
       case "PUT":
         res.status(200).json({ message: "PUT method" });
