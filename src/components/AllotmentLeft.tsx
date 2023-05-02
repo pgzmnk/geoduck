d704c684396d22e711337feb62685db467fa1106
pgzmnk <pgzmnk@gmail.com>
2023-05-02 14:09:09 -0700

    Layers styling.

--- src/components/AllotmentLeft.tsx
+++ src/components/AllotmentLeft.tsx
@@ -50,56 +50,67 @@ const Layers = () => {
 
   // return mapping of layers
   return (
-    <div className="flex-1">
-      <Button
-        size="sm"
-        color="white"
-        className="flex items-center gap-3 outline outline-offset-2 outline-2	hover:outline-4"
-        onClick={() => {
-          setCollapsed(!collapsed);
-        }}
-      >
-        {collapsed ? (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            class="w-6 h-6"
+    <div className="flex-column">
+      <div class="flex flex-row">
+        <div>
+          <Button
+            size="sm"
+            color="white"
+            className="flex items-center gap-3 outline outline-offset-2 outline-2	hover:outline-4"
+            onClick={() => {
+              setCollapsed(!collapsed);
+            }}
           >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M4.5 15.75l7.5-7.5 7.5 7.5"
-            />
-          </svg>
-        ) : (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            class="w-6 h-6"
+            {collapsed ? (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-6 h-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M4.5 15.75l7.5-7.5 7.5 7.5"
+                />
+              </svg>
+            ) : (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-6 h-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+                />
+              </svg>
+            )}
+            Map Layers
+          </Button>
+        </div>
+        <div class="px-4">
+          <IconButton size="md" color="pink"
           >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M19.5 8.25l-7.5 7.5-7.5-7.5"
-            />
-          </svg>
-        )}
-        Map Layers
-      </Button>
+            <i className="fas fa-solid fa-square-plus" />
+          </IconButton>
+        </div>
+
+      </div >
       <div class="py-10 pl-5">
         {!collapsed
           ? Object.keys(layers).map((key, index) => (
-              <LayerCard layer={layers[key]} key={key} />
-            ))
+            <LayerCard layer={layers[key]} key={key} />
+          ))
           : null}
       </div>
-    </div>
+    </div >
   );
 };
 
