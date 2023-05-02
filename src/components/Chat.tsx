6360811e0b6071608b9fd2908f55c4a0a6224205
pgzmnk <pgzmnk@gmail.com>
2023-05-02 16:00:18 -0700

    Update base chat.

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 
-interface ChatProps {}
+interface ChatProps { }
 
 export const Chat = (props: ChatProps) => {
   return (
     <div>
-      <h1>Chat</h1>
+      <div className="chat chat-start">
+        <div className="chat-bubble">Rendering polygons...<br />Successfully rendered polygons.</div>
+      </div>
+      <div className="chat chat-end">
+        <div className="chat-bubble">Test command.</div>
+      </div>
     </div>
   );
 };
