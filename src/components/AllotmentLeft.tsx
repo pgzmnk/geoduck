3a72a4f4bd9d466e6e7c83bdcc8cdeb71ad253bf
pgzmnk <pgzmnk@gmail.com>
2023-05-02 17:09:46 -0700

    Modal z-20.

--- src/components/AllotmentLeft.tsx
+++ src/components/AllotmentLeft.tsx
@@ -1,9 +1,11 @@
-import React, { useState, useRef, useCallback, useEffect } from "react";
+import React, { useState, Fragment } from "react";
 import { IconButton } from "@material-tailwind/react";
 import { useContext } from "react";
 import { MapContext } from "@/context/context";
 import * as rd from "@duckdb/react-duckdb";
 import { renderMapData } from "@/utils/mapFunctions";
+import { Dialog, Transition } from '@headlessui/react'
+
 
 import {
   Tabs,
@@ -50,6 +52,15 @@ const Layers = () => {
     { name: "ent", type: "polygon", tableName: "ent" },
   ]);
   const [collapsed, setCollapsed] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+
+  function closeModal() {
+    setIsOpen(false)
+  }
+
+  function openModal() {
+    setIsOpen(true)
+  }
 
   // render layers on map
   layers.map((layer) => {
@@ -118,45 +129,76 @@ const Layers = () => {
           </Button>
         </div>
         <div class="px-4">
-          <label htmlFor="my-modal-3" className="btn">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              class="w-6 h-6"
+          <IconButton
+            onClick={openModal}>
+            <i className="fas fa-plus" />
+          </IconButton>
+          <Transition appear show={isOpen} as={Fragment}>
+            <div
+              class="fixed "
             >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
-              />
-            </svg>
-          </label>
+              <Dialog as="div" className="relative z-20" onClose={closeModal}>
+                <Transition.Child
+                  as={Fragment}
+                  enter="ease-out duration-300"
+                  enterFrom="opacity-0"
+                  enterTo="opacity-100"
+                  leave="ease-in duration-200"
+                  leaveFrom="opacity-100"
+                  leaveTo="opacity-0"
+                >
+                  <div className="fixed inset-0 bg-black bg-opacity-25" />
+                </Transition.Child>
+
+                <div className="fixed inset-0 overflow-y-auto">
+                  <div className="flex min-h-full items-center justify-center p-4 text-center">
+                    <Transition.Child
+                      as={Fragment}
+                      enter="ease-out duration-300"
+                      enterFrom="opacity-0 scale-95"
+                      enterTo="opacity-100 scale-100"
+                      leave="ease-in duration-200"
+                      leaveFrom="opacity-100 scale-100"
+                      leaveTo="opacity-0 scale-95"
+                    >
+                      <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                        <Dialog.Title
+                          as="h3"
+                          className="text-lg font-medium leading-6 text-gray-900"
+                        >
+                          Payment successful
+                        </Dialog.Title>
+                        <div className="mt-2">
+                          <p className="text-sm text-gray-500">
+                            Your payment has been successfully submitted. We’ve sent
+                            you an email with all of the details of your order.
+                          </p>
+                        </div>
+
+                        <div className="mt-4">
+                          <button
+                            type="button"
+                            className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                            onClick={closeModal}
+                          >
+                            Got it, thanks!
+                          </button>
+                        </div>
+                      </Dialog.Panel>
+                    </Transition.Child>
+                  </div>
+                </div>
+              </Dialog>
+            </div>
+          </Transition>
+
         </div>
+
       </div>
       <div class="py-10 pl-5">
         {!collapsed
           ? layers.map((layer) => <LayerCard key={layer.name} layer={layer} />)
           : null}
-      </div>
-      <input type="checkbox" id="my-modal-3" className="modal-toggle" />
-      <div className="modal z-50">
-        <div className="modal-box relative">
-          <label
-            htmlFor="my-modal-3"
-            className="btn btn-sm btn-circle absolute right-2 top-2"
-          >
-            ✕
-          </label>
-          <h3 className="text-lg font-bold">
-            test
-          </h3>
-          <p className="py-4">
-            test
-          </p>
-        </div>
       </div>
     </div>
   );
