7465072f074534d81cf48cc7b9230f9e804adc76
pgzmnk <pgzmnk@gmail.com>
2023-05-23 11:29:58 -0700

    Update console.log. wip

--- src/utils/duckdbFunctions.tsx
+++ src/utils/duckdbFunctions.tsx
@@ -20,13 +20,12 @@ export async function runQueryDuckDb(db: any, query: string) {
   try {
     const c = await db!.value!.connect();
     const response = await c.query(query);
-    console.log("response.toString()", response.toString())
+    console.log("query: ", query, "\nresponse.toString(): ", response.toString());
     return response.toString();
   } catch (error) {
     // Show error in console to helps the user debug. In the future, the error should be surfaced to the UI.
     console.log("error: ", error);
-    console.log("- running duckdbquery...")
-    setTimeout(runQueryDuckDb, 50000, db, query);
-    // runQueryDuckDb(db, query);
+    console.log("- running duckdbquery...");
+    setTimeout(runQueryDuckDb, 20000, db, query);
   }
 }
