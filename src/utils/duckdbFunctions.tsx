839ff53a1df1523e712967492d62767c2d689847
pgzmnk <pgzmnk@gmail.com>
2023-05-23 10:35:54 -0700

    wip Polygons show.

--- src/utils/duckdbFunctions.tsx
+++ src/utils/duckdbFunctions.tsx
@@ -1,10 +1,10 @@
+import React, { useState } from "react";
 import * as rd from "@duckdb/react-duckdb";
 
 export async function LoadInitialData() {
   const db = rd.useDuckDB();
 
   var tableNames = [];
-
   fetch("/api/v0/table")
     .then((res) => res.json())
     .then((res) => {
@@ -14,24 +14,14 @@ export async function LoadInitialData() {
         tableNames.push(table.name);
       });
     });
-
   return tableNames;
 }
 
 export async function runQueryDuckDb(db: any, query: string) {
-  async function wait() {
-    console.log("waiting for db connection...");
-    if (db!.value!) {
-      setTimeout(wait, 2000);
-    } else {
-      const c = await db!.value!.connect();
-      const response = await c.query(query);
-      console.log("obtained duckdb response: ", response.toString());
-      return response.toString();
-    }
-  }
   try {
-    wait();
+    const c = await db!.value!.connect();
+    const response = await c.query(query);
+    return response.toString();
   } catch (error) {
     // Show error in console to helps the user debug. In the future, the error should be surfaced to the UI.
     console.log("error: ", error);
